6d7dc9a134dae7283277cf086f9296463f545c4b
Igor Lemes <igor.lemes@live.com>
2020-01-20 23:25:25 -0300

    Display component add

---
+++ src/components/Display.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import './Display.css'
+
+export default pros =>
+<div className="display">100</div>
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,11 +1,15 @@
 import React , { Component} from  'react'
 import './Calculator.css'
 import Button from '../components/Button'
+import Display from '../components/Display'
+
 
 export default class Calculator extends Component {
     render() {
         return (
             <div className="calculator">
+                <Display/>
+
                 <Button label='AC'/>
                 <Button label='/'/>
                 <Button label='7'/>
